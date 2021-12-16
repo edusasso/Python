b1149dedc2402837b4f19ea325384cfd39f4a457
Eduardo Sasso <edu_sasso@hotmail.com>
2021-12-16 10:25:48 -0300

    exercícios feitos

---
+++ ex066_numeros_com_flag.py
@@ -0,0 +1,9 @@
+c = s = 0
+while True:
+    n = int(input('Digite um valor [999 para parar]: '))
+    c += 1
+    if n == 999:
+        break
+    s += n
+print(f'A soma dos {c} valores foi {s}!')
+    
